ac8022baf47105836a9aea44ea667280db05b453
Devika Mujgule <mujgule.d@northeastern.edu>
2023-12-12 01:28:31 -0500

    console logs cleanup

--- bloc.js
+++ bloc.js
@@ -2,9 +2,7 @@
 const buttonClickArray = [];
 
 const changeColor = (id) => {
-  console.log("inside chang color funtion")
   buttonClickArray.push(id);
-  console.log(buttonClickArray);
 
   const clickedBlockId = document.getElementById(id);
   clickedBlockId.style.backgroundColor = "#52AA5E";
@@ -21,14 +19,9 @@ const reverseColor = () => {
     if (buttonClickArray.length == 0) {
       clearInterval();
     } else {
-      console.log(`buttonClickArray`, buttonClickArray.length);
       const clickedBlockId = document.getElementById(buttonClickArray.pop());
       clickedBlockId.style.backgroundColor = "#F6F7EB";
     }
   }, 1000);
 
 }
-
-
-
-console.log(buttonClickArray, buttonClickArray.length);
